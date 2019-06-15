7e6ccba8b238bbfe5626a1c6c90131b8a1afee9e
sandrine <sandrinezocca@gmail.com>
2019-06-15 16:07:44 +0200

    Gérer la soumission du formulaire de création d'exercice

--- src/components/TrainingDayDetails.svelte
+++ src/components/TrainingDayDetails.svelte
@@ -6,7 +6,13 @@
         reps: 10
     }
 
-    let exercices = [];
+    let exercises = [];
+
+    const handleSubmit = () => {
+        exercises = [...exercises, { ...exercise }];
+        console.log(exercises);
+        
+    }
 
 </script>
 
@@ -14,4 +20,21 @@
 
 <div>
     <h2>{ title }</h2>
+     <div>
+    <form on:submit|preventDefault={handleSubmit}>
+      <div>
+        <label for="activity">Activity</label>
+        <input type="text" id="activity" bind:value={exercise.activity} />
+      </div>
+      <div>
+        <label for="sets">Sets</label>
+        <input type="number" id="sets" bind:value={exercise.sets} />
+      </div>
+      <div>
+        <label for="reps">Reps</label>
+        <input type="number" id="reps" bind:value={exercise.reps} />
+      </div>
+      <button type="submit">add exercise</button>
+    </form>
+  </div>
 </div>
