725881aba8910dae3417f66a1b51c109e49d77fe
sandrine <sandrinezocca@gmail.com>
2019-06-15 15:47:21 +0200

    Ajouter dynamiquement une ou plusieurs instances de TrainingDay

--- src/components/TrainingDay.svelte
+++ src/components/TrainingDay.svelte
@@ -1,12 +1,14 @@
 <script>
+  import TrainingDayDetails from './TrainingDayDetails.svelte'
+
   const nbOfDays = [
-    { id: 1, text: "One day", value: 1 },
-    { id: 2, text: "Two days", value: 2 },
-    { id: 3, text: "Three days", value: 3 },
-    { id: 4, text: "Four day", value: 4 },
-    { id: 5, text: "Five days", value: 5 },
-    { id: 6, text: "Six days", value: 6 },
-    { id: 7, text: "Seven day", value: 7 }
+    { id: 1, text: "One day", value: [1] },
+    { id: 2, text: "Two days", value: [1, 2] },
+    { id: 3, text: "Three days", value: [1, 2, 3] },
+    { id: 4, text: "Four day", value: [1, 2, 3, 4] },
+    { id: 5, text: "Five days", value: [1, 2, 3, 4, 5] },
+    { id: 6, text: "Six days", value: [1, 2, 3, 4, 5, 6] },
+    { id: 7, text: "Seven day", value: [1, 2, 3, 4, 5, 6, 7] }
   ];
 
   let ndDaysTrainingPerWeek;
@@ -20,9 +22,19 @@
   <form>
     <select bind:value={ndDaysTrainingPerWeek}>
       {#each nbOfDays as day (day.id)}
-        <option value={day}>{day.text}</option>
+        <option value={day}>{day.text} per week</option>
       {/each}
     </select>
   </form>
    {JSON.stringify(ndDaysTrainingPerWeek)}
 </div>
+
+{#if ndDaysTrainingPerWeek}
+<div>
+  {#each ndDaysTrainingPerWeek.value as dayNumber, i}
+    <div>
+      <TrainingDayDetails title={ `day ${++i}` } />
+    </div>
+  { /each }
+</div>
+{/if}
---
+++ src/components/TrainingDayDetails.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let title;
+    let exercise = {
+        activity: "",
+        sets: 4,
+        reps: 10
+    }
+
+    let exercices = [];
+
+</script>
+
+<style></style>
+
+<div>
+    <h2>{ title }</h2>
+</div>
