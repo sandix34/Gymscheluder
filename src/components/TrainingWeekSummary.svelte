5135d3d7509b376d2194d58f20106d56a0ad0fcf
sandrine <sandrinezocca@gmail.com>
2019-08-03 17:43:12 +0200

    Préparation du résumé en utilisant reduce()

--- src/components/TrainingWeekSummary.svelte
+++ src/components/TrainingWeekSummary.svelte
@@ -1,3 +1,27 @@
-<script></script>
+<script>
+    export let summary;
+    export let days;
+    let formattedData;
+    let isDataFormated = false;
+
+    const prepareSummary = () => {
+        formattedData = summary.reduce((acc, curr)=>{
+            if (Object.keys(acc).includes(curr.title)) {
+                acc[curr.title] = [...acc[curr.title], curr]
+            } else {
+                acc[curr.title] = [curr];
+            }
+            return acc;
+        }, {});
+        isDataFormated = true;
+        console.log(formattedData);
+        
+    }
+
+    prepareSummary();
+</script>
 <style></style>
 <h2>Summary</h2>
+{#if isDataFormated}
+{formattedData}
+{/if}
