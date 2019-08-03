8fd68866d88d743785bd2214110f6cf22698b1ea
sandrine <sandrinezocca@gmail.com>
2019-08-03 18:19:38 +0200

    Rendu du résumé détaillé du programme hebdomadaire

--- src/components/TrainingWeekSummary.svelte
+++ src/components/TrainingWeekSummary.svelte
@@ -1,31 +1,43 @@
 <script>
-    import { onMount} from 'svelte';
-    export let summary;
-    export let days;
-    let formattedData;
-    let isDataFormated = false;
+  import { onMount } from "svelte";
+  export let summary;
+  export let days;
+  let formattedData;
+  let isDataFormated = false;
 
-    const prepareSummary = () => {
-        formattedData = summary.reduce((acc, curr)=>{
-            if (Object.keys(acc).includes(curr.title)) {
-                acc[curr.title] = [...acc[curr.title], curr]
-            } else {
-                acc[curr.title] = [curr];
-            }
-            return acc;
-        }, {});
-        isDataFormated = true;
-        console.log(formattedData);
-        
-    }
+  const prepareSummary = () => {
+    formattedData = summary.reduce((acc, curr) => {
+      if (Object.keys(acc).includes(curr.title)) {
+        acc[curr.title] = [...acc[curr.title], curr];
+      } else {
+        acc[curr.title] = [curr];
+      }
+      return acc;
+    }, {});
+    isDataFormated = true;
+    console.log(formattedData);
+  };
 
-    onMount(()=> {
-        prepareSummary();
-    })
-    
+  onMount(() => {
+    prepareSummary();
+  });
 </script>
-<style></style>
-<h2>Summary</h2>
+
+<style>
+
+</style>
+
 {#if isDataFormated}
-{formattedData}
+  <h2>Summary</h2>
+  {#if summary.length}
+    {#each Object.keys(formattedData) as day}
+      <h2>{day}</h2>
+      {#each formattedData[day] as ex}
+        {ex.activity}: {ex.sets} set of {ex.reps} reps
+        <br />
+      {/each}
+    {/each}
+  {:else}
+    <div>Missing data: please add exercises</div>
+  {/if}
 {/if}
